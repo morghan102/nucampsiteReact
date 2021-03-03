b10dd360b02117f460999368f4acc52e3121dfce
Morghan <morghan102@gmail.com>
2021-03-03 15:02:23 -0800

    Components part 2

---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Directory extends Component {
+    // component is the parent class thats from the react library
+    constructor(props) {
+        super(props);
+        this.state = {
+              selectedCampsite: null
+        };
+    }
+
+    onCampsiteSelect(campsite) {
+        this.setState({selectedCampsite: campsite});
+        // cannot set state directly (as in this.state.selectedCampsite = campsite) -- MUST use setState
+    }
+
+    renderSelectedCampsite(campsite) {
+        if (campsite) {
+            return (
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        return <div />;
+    }
+
+    render() {
+        const directory = this.props.campsites.map(campsite => {
+            return (
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {directory}
+                </div>
+                <div className="row">
+                    <div class="col-md-5 m-1">
+                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Directory;
